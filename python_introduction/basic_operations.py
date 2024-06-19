6304bcdbcf2f089123a88d07b275f1df306f0336
itsmearafik <arafikalhassan@gmail.com>
2024-06-19 17:27:42 +0100

    Basic Arithmetic Exercise in Python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# Basic Arithmetic Operations in Python
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+sub = number2 + number1
+mult = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum}") 
+print(f"Subtraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {mult}")
