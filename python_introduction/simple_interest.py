e2fab3ca08964b8052da5f90e9703be636c1aabe
itsmearafik <arafikalhassan@gmail.com>
2024-06-19 17:53:59 +0100

    corrected the variable assignments issue and the correct calculation of the simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,10 +2,10 @@
 # The formula for simple interest is (I = P * R * T) 
 # Where I is Interest, P is Principle, R is annual interest, T is time in years
 
-principle = 1000
+principal = 1000
 rate = 0.05
 time = 3
 
-interest = principle * rate * time
+simple_interest = (principal * rate * time)
 
-print(f"The simple interest is: {interest}")
+print(f"The simple interest is: {simple_interest}")
