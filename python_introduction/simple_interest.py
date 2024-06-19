b86134a6b397a4da5a37cd7a30c1ffbdfdcd7def
itsmearafik <arafikalhassan@gmail.com>
2024-06-19 17:48:05 +0100

    Added the Simple interest calculator feature, with principal of 1000, rate of 5% and time of 3 years

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Calculating the interest on a given principal amount
+# The formula for simple interest is (I = P * R * T) 
+# Where I is Interest, P is Principle, R is annual interest, T is time in years
+
+principle = 1000
+rate = 0.05
+time = 3
+
+interest = principle * rate * time
+
+print(f"The simple interest is: {interest}")
