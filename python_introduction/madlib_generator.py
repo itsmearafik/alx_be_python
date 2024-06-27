2e877c5f9d9cecc391fda873d57de9e91410b2dd
itsmearafik <arafikalhassan@gmail.com>
2024-06-27 07:52:49 +0100

    Created the madlib generator file

---
+++ python_introduction/madlib_generator.py
@@ -0,0 +1,12 @@
+# Mad Libs Generator 
+
+'''
+Task to complete project 
+1. Prompt the user for input - verb, adjective, noun
+2. Store the user's input 
+3. Build the story - create a story template
+4. Optional (Conditional inputs) - variations based on user inputs
+5. Print final story - 
+
+'''
+
