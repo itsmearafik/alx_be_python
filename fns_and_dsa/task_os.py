c1305cb827443cc32401f52a0991da9b44f1b9be
itsmearafik <arafikalhassan@gmail.com>
2024-07-19 16:16:17 +0100

    Used the os library to get the current working directory

---
+++ fns_and_dsa/task_os.py
@@ -0,0 +1,7 @@
+# an example to interact with the os, using the os library 
+# importing the os library
+import os 
+
+# get the current working directory
+cwd = os.getcwd()
+print(f"The current working directpory is: {cwd}")
