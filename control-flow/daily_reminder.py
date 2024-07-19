2822652362b0f817e6b99f6ee6bb9c15aad23b9d
itsmearafik <arafikalhassan@gmail.com>
2024-07-19 12:16:25 +0100

    Updated the input checker.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,21 +2,34 @@
 
 # Asking for user inputs 
 # store user input task description as => task_variable
-# Ask for priority level (high, medium,low) as => priority_variable 
-# Ask if its time bound (Yes/No) => time_bound_variable
-
 task_variable = input("Enter your task: ")
+
+# Ask for priority level (high, medium,low) as => priority_variable 
 priority = input("Priority (high/medium/low): ")
+
+# Ask if its time bound (Yes/No) => time_bound_variable
 time_bound = input("Is it time-bound? (yes/no): ")
 
-# Process task based on priority and time sensitivity
+# Provide a customized reminder based on the priority and time sensitivity
+reminder = ""
 
-match time_bound:
-    case time_bound if ("yes" in time_bound):
-        # if time_bound == "yes":
-        print(f"Reminder: '{task_variable}' is a {priority} priority task that requires immidiate attention today!")
-    case time_bound if (time_bound == "no"):
-        print(f"Reminder: '{task_variable}' is a {priority} priority task. Consider completing it when you have free time.")
+# Use match case to handle inputs based on priorities 
+match priority:
+    case "high":
+        reminder = f"{task_variable} is a high priority task."
+    case "medium":
+        reminder = f"{task_variable} is a medium priority task."
+    case "low":
+        reminder = f"{task_variable} is a low priority task."
     case _:
-        print("Please enter the correct response. check sentence for example")
+        reminder = f"{task_variable} has an unspecified priority level."
 
+# Use if conditionals to modify the reminder if the task is time bound 
+if time_bound.lower() == "yes":
+    reminder += " that requires immediate attention today!"
+else:
+    reminder += " Consider completing it when you have free time."
+
+
+# Print the customized reminder 
+print("Reminder: ", reminder)
