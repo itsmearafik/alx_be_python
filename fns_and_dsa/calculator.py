3d326819b2c5a5285b240fc64f4bc612fc90fd6b
itsmearafik <arafikalhassan@gmail.com>
2024-07-19 15:53:28 +0100

    Explored the use of python modules by creating a calculator module

---
+++ fns_and_dsa/calculator.py
@@ -0,0 +1,15 @@
+# math functions module 
+
+def add(x, y):
+    """Return the sum of two numbers."""
+    return x + y 
+def substract(x, y):
+    """Return the difference of two numbers"""
+    return x - y 
+
+def multiplication(x, y):
+    """Return the product of two numbers."""
+    return x * y 
+
+def divider(x, y):
+    return x / y 
---
+++ fns_and_dsa/module_main.py
@@ -0,0 +1,15 @@
+# main.py (main program)
+# importing math_functions module 
+import fns_and_dsa.calculator as calculator
+
+result = calculator.add(5,3)
+print(result)
+
+result = calculator.substract(5, 3)
+print(result)
+
+result = calculator.multiplication(5, 3)
+print(result)
+
+result = calculator.divider(8, 4)
+print(result)
