4ad5cc42fef7f8db6cb3272a149f135afea65500
itsmearafik <arafikalhassan@gmail.com>
2024-07-20 14:07:16 +0100

    Updated the implementation of the shopping_list array

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,42 +1,33 @@
-""" 
-Objective: Utilize Python lists to create a simple shopping list manager that allows users to add items, view the current list, and remove items.
-"""
+# shopping_list_manager.py
 
-# definition of the shopping_list_manager display_menu
 def display_menu():
-    print("Shopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
     print("4. Exit")
 
-
-# implementation of the shopping_list_manager function 
 def main():
-    shoppping_list = []
+    shopping_list = []
     while True:
         display_menu()
         choice = input("Enter your choice: ")
 
         if choice == '1':
             item = input("Enter the item to add: ")
-            shoppping_list.append(item)
+            shopping_list.append(item)
             print(f"'{item}' has been added to the list.")
-        elif choice == "2":
+        elif choice == '2':
             item = input("Enter the item to remove: ")
-            if item in shoppping_list:
-                shoppping_list.remove(item)
+            if item in shopping_list:
+                shopping_list.remove(item)
                 print(f"'{item}' has been removed from the list.")
             else:
                 print(f"'{item}' not found in the list.")
-        elif choice == "3":
-            if shoppping_list:
-                print("Current Shopping List: ")
-                for i, item in enumerate(shoppping_list, 1):
+        elif choice == '3':
+            if shopping_list:
+                print("Current Shopping List:")
+                for i, item in enumerate(shopping_list, 1):
                     print(f"{i}. {item}")
             else:
                 print("The shopping list is currently empty.")
-        elif choice == "4":
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
