c963742634ed99329f735a62159e232f6ec68ae4
itsmearafik <arafikalhassan@gmail.com>
2024-07-20 16:13:40 +0100

    Changed the output from 2dp to 1dp.

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -16,5 +16,5 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
-    
-    return f"The result of the division is {result:.2f}"
+
+    return f"The result of the division is {result:.1f}"
