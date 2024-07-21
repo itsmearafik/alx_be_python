e3c8874b825d86d4660e6bacd4583d5afd3cf84e
itsmearafik <arafikalhassan@gmail.com>
2024-07-21 12:37:57 +0100

    Added a test script to check the correct implementation of the arrithmetic calculator operations.

---
+++ programming_paradigm/simple_calculator.py
@@ -0,0 +1,28 @@
+""" 
+Objective: Writing a unit test for a this `Simple Calculator` class that supports
+addition, substraction, multiplication and division operations.
+"""
+class SimpleCalculator:
+    #Addition operation
+    def add(self, a, b):
+        # return the addition of a and b 
+        return a + b 
+    
+    # Substraction operation 
+    def subtract(self, a, b):
+        # return the difference of a and b
+        return a - b 
+    
+    # Multiplication of a and b 
+    def multiply(self, a,b):
+        # return the multiplication of a and b 
+        return a * b 
+    
+    # Division of a and b 
+    def divide(self, a, b):
+        # return the division of a by b. Return none if b is zero 
+        if b == 0:
+            return None 
+        else:
+            return a / b 
+        
---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,48 @@
+""" 
+Objective: Writing a unit test for a this `Simple Calculator` class that supports
+addition, substraction, multiplication and division operations.
+"""
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+
+        # Set up the SimpleCalculator instance beforeeach test.
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    
+        # Test the addition method.
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2,3), 5)
+        self.assertEqual(self.calc.add(-1,1), 0)
+        self.assertEqual(self.calc.add(0,0), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+
+        # Test the subtraction method 
+    def test_substraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 1), -1)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+
+    # Test the multiplication method 
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(0, 1), 0)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+
+    # Test the Division method 
+    def test_division(self):
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        self.assertEqual(self.calc.divide(-6, -3), 2)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
